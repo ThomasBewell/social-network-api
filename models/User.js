d461f09a0e98b15743c0c877f2ebb002b7b33629
Thomas Bewell <tombewell@gmail.com>
2021-09-30 16:17:50 -0500

    add user and thought models and date format

---
+++ models/User.js
@@ -0,0 +1,46 @@
+const { Schema, model } = require('mongoose');
+
+const UserSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: "Username is required",
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: "E-mail is required",
+        },
+        // array of _id values that reference the Thought model
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            }
+        ],
+        // array of _id values that reference the User model
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            }
+        ]
+    },
+    {
+        toJSON: {
+            virtuals: true
+        },
+        id: false
+    }
+);
+
+// get friend count
+UserSchema.virtual('friendCount').get(function() {
+  return this.friends.length;
+});
+
+const User = model('User', UserSchema);
+
+module.exports = User;
