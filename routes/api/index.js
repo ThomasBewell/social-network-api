ef345a07d55e44cfc1bcbf58e4a6e2808809c772
Thomas Bewell <tombewell@gmail.com>
2021-09-30 21:10:24 -0500

    add user routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,9 @@
 const router = require('express').Router();
-const apiRoutes = require('./api');
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
 
-router.use('/api', apiRoutes);
-
-router.use((req, res) => {
-    res.status(404).send('<h1>404 Error</h1>');
-});
+// add prefix `/users` to routes created in user-routes.js
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
---
+++ routes/api/user-routes.js
@@ -0,0 +1,35 @@
+const router = require('express').Router();
+
+const {
+    getAllUser,
+    getUserById,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    removeFriend
+} = require('../../controllers/user-controller')
+
+// GET all and POST at /api/users
+router
+    .route('/')
+    .get(getAllUser)
+    .post(createUser);
+
+// GET one, PUT, and DELETE at /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// api/users/:userId/friends/:friendId
+router
+    .route('/:id/friends/:friendId')
+    .put(addFriend);
+  
+router
+    .route('/:id/friends/:friendId')
+    .delete(removeFriend);
+
+module.exports = router;
